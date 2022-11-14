899677c87a58b0a6a460858a7e456c80ce74d1cd
mohamadhandy <108653540+mohamadhandy@users.noreply.github.com>
2022-11-14 16:46:34 +0700

    filtered users using new state

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,40 @@ class App extends Component {
     super();
     this.state = {
       users: [],
+      filteredUsers: [],
     };
   }
   async componentDidMount() {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const users = await res.json();
     this.setState(() => {
-      return { users };
+      return {
+        users,
+        filteredUsers: users,
+      };
     });
   }
   render() {
     return (
       <div className="App">
-        {this.state.users.map((user, index) => (
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search users"
+          onChange={(e) => {
+            const filteredUsers = this.state.filteredUsers.filter((user) => {
+              return user.name
+                .toLowerCase()
+                .includes(e.target.value.toLowerCase());
+            });
+            this.setState(() => {
+              return e.target.value.toLowerCase()
+                ? { filteredUsers }
+                : { filteredUsers: this.state.users };
+            });
+          }}
+        />
+        {this.state.filteredUsers.map((user, index) => (
           <p key={index} className="read-the-docs">
             My name is {user.name} and I work at {user.company.name}
           </p>
